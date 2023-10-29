4e7f700af113284cbc341cb30b5e0f7622cb98a7
Nekyl <88288807+Nekyl@users.noreply.github.com>
2023-10-29 10:59:34 -0300

    Update NekoTransfer_2.0.py

--- NekoTransfer_2.0.py
+++ NekoTransfer_2.0.py
@@ -1,151 +1,69 @@
+# ------------------- MASSIVE COINS  TRANSFER v2.0 -------------------
+
 import getpass
-
+from threading import Thread
+import aminofix
 import pyfiglet
-
-
+import os
+import time
+from progress.bar import IncrementalBar
 
 def colored(r : int = None, g : int = None, b : int = None, rb : int = None, gb : int = None, bb : int = None, text = None):
 
-
-
     if rb is None and gb is None and bb is None:
-
         return "\033[38;2;{};{};{}m{}\033[0m".format(r, g, b, text)
-
     elif r is None and g is None and b is None:
-
         return "\033[48;2;{};{};{}m{}\033[0m".format(rb, gb, bb, text)
-
     else:
-
         return "\033[38;2;{};{};{}m\033[48;2;{};{};{}m{}\033[0m".format(r, g, b, rb, gb, bb, text)
 
 print("\n" + colored(r = 180, g=197, b=49, text=f"""{pyfiglet.figlet_format("NekoTransfer", font="standard")}"""))
 
-
-
 print("   " + colored(r=17, g=10, b=3, rb=251, gb=197, bb=49, text=" ❯ ") + colored(rb=99, gb=13, bb=28, r=251, g=197, b=49, text=" Release 2.0 ") + "         " + colored(r=17, g=10, b=3, rb=251, gb=197, bb=49, text=" ❮ Made by: Neko ❯ ") + "\n")
 
-
+client = aminofix.Client()
 
 email = input("\n " + colored(r=17, g=10, b=3, rb=251, gb=197, bb=49, text=" ❯ ") + colored(rb=99, gb=13, bb=28, r=251, g=197, b=49, text=" Insira seu e-mail: ") + " ")
 
-
-
 password = getpass.getpass("\n " + colored(r=17, g=10, b=3, rb=251, gb=197, bb=49, text=" ❯ ") + colored(rb=99, gb=13, bb=28, r=251, g=197, b=49, text=" Digite sua senha: ") + " ")
 
+link = input("\n " + colored(r=17, g=10, b=3, rb=251, gb=197, bb=49, text=" ❯ ") + colored(rb=99, gb=13, bb=28, r=251, g=197, b=49, text=" Insira o link do blog: ") + " ")
 
+client.login(email, password)
 
-link = input("\n " + colored(r=17, g=10, b=3, rb=251, gb=197, bb=49, text=" ❯ ") + colored(rb=99, gb=13, bb=28, r=251, g=197, b=49, text=" Insira o link do perfil : ") + " ")
+parsed = client.get_from_code(link)
+comId = parsed.comId
+blog = parsed.objectId
 
+subclient = aminofix.SubClient(comId = comId, profile = client.profile)
 
+print("\n " + colored(r=17, g=10, b=3, rb=251, gb=197, bb=49, text=" ❮ LOGADO ! ❯                                           "))
 
-send = int(input("\n " + colored(r=17, g=10, b=3, rb=251, gb=197, bb=49, text=" ❯ ") + colored(rb=99, gb=13, bb=28, r=251, g=197, b=49, text=" Digite o valor: ")) + " ")
+print("\n  " + colored(r=181, g=45, b=27, text=" Coin balance ") + colored(r=251, g=197, b=49, text="antes ") + colored(r=181, g=45, b=27, text="Seus coins ") + colored(r=251, g=197, b=49, text=f"{int(client.get_wallet_info().totalCoins)}"))
 
+amount = int(input("\n " + colored(r=17, g=10, b=3, rb=251, gb=197, bb=49, text=" ❯ ") + colored(rb=99, gb=13, bb=28, r=251, g=197, b=49, text=" Insira o valor: ") + " "))
 
+print("\n " + colored(r=17, g=10, b=3, rb=251, gb=197, bb=49, text=" ❮ TRANSFERINDO COINS ... ❯                                ") + "\n\n")
 
-try:
+divided = (amount // 500)
 
-	import samino
+bar = IncrementalBar('   Transferindo', max=divided + 1)
 
-	from tqdm import tqdm
+for _ in range(divided):
+    Thread(target=subclient.send_coins, args=(500, blog)).start()
+    time.sleep(0.30)
+    bar.next()
 
-except:
+little_sum = amount % 500
+if little_sum != 0:
+    subclient.send_coins(little_sum, blog)
+    bar.next()
 
-	import os
+bar.finish()
 
-	os.system("pip install aiohttp samino==2.5.2 tqdm")
+print("\n\n " + colored(r=17, g=10, b=3, rb=251, gb=197, bb=49, text=" ❮ TRANSFERÊNCIA FINALIZADA ! ❯                               "))
 
-	import samino
+print("\n  " + colored(r=181, g=45, b=27, text=" Coin balance ") + colored(r=251, g=197, b=49, text="depois ") + colored(r=181, g=45, b=27, text="Seus coins ") + colored(r=251, g=197, b=49, text=f"{int(client.get_wallet_info().totalCoins)}") + "\n")
 
-	from tqdm import tqdm
-
-
-
-
-
-count = 0
-
-def subscribe(userId):
-
-	try:
-
-		global count
-
-		subclient.subscribe(userId)
-
-		count += 500
-
-
-
-	except:
-
-
-
-		subscribe(userId)
-
-		
-
-colored_text1 = "         " + colored(r=17, g=10, b=3, rb=251, gb=197, bb=49, text=" ❯ ") + colored(rb=99, gb=13, bb=28, r=251, g=197, b=49, text=" [1] Entrar no Fã Clube")
-
-colored_text2 = "         " + colored(r=17, g=10, b=3, rb=251, gb=197, bb=49, text=" ❯ ") + colored(rb=99, gb=13, bb=28, r=251, g=197, b=49, text=" [2] Líder comunidade")
-
-
-
-print("\n" + colored_text1 + "\n" + colored_text2 + "\n")
-
-
-
-
-
-
-
-x = int(input("Selecione: "))
-
-if x == 1 or x == 2:
-
-	print("\033[1;40;44m Transfer by Neko 2.0\033[0m")
-
-	client = samino.Client()
-
-	mult = client.get_from_link(link)
-
-	comId = mult.comId
-
-	userId = mult.objectId
-
-	client.login(email, password)
-
-	coins = client.get_wallet_info().totalCoins
-
-	subclient = samino.Local(comId)
-
-	coins = client.get_wallet_info().totalCoins
-
-	print(f"\nSeus coins: {coins}\n")
-
-	if x == 2:
-
-		acm = samino.Acm(comId)
-
-	if x == 2:
-
-		acm.add_influencer(userId, monthlyFee = 500)
-
-	count = 0
-
-	print()
-
-	for i in tqdm(range(send//500)):
-
-		subscribe(userId)
-
-	print(f"Falha ao enviar: {send - count}")
-
-	if x == 2:
-
-		acm.remove_influencer(userId)
-
-else:
-
-	print("Erro: Opção inválida")
+client.logout()
+os._exit(0)
